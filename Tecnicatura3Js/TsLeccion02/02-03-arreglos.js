84e9f8bd8768847f6adbc3978134f6bfc561dcd6
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-20 20:17:42 -0300

    Arreglos

--- Tecnicatura3Js/TsLeccion02/02-03-arreglos.js
+++ Tecnicatura3Js/TsLeccion02/02-03-arreglos.js
@@ -2,3 +2,29 @@
 const autos =['Ferrari','Renault','BMW'];
 console.log (autos);
 
+//Recorremos los elementos de un arreglo 
+console.log(autos[0]);
+console.log(autos[2]);
+
+for(let i = 0; i < autos.length; i++){
+    console.log  (i+' : '+autos[i]);  
+} 
+
+//Modificamos los elementos del arreglo
+autos[1]='Volvo';
+console.log(autos[1]);
+
+//Agregamos nuevos valores al arreglo
+autos.push('Audi');//Agregar el elemento al final del arreglo
+console.log(autos)
+
+//Otras formas de agragar elementos al arreglo
+autos[autos.length]='Porche';
+console.log(autos);
+
+//Tercera forma de agregar elementos teniendo CUIDADO
+autos[6] = 'Renaul';
+console.log(autos);
+
+
+
