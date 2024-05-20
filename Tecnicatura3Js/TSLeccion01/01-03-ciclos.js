fc5c87afdf59bcd9a941068bef46294e40ee74db
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-20 10:53:13 -0300

    Palabra break

--- Tecnicatura3Js/TSLeccion01/01-03-ciclos.js
+++ Tecnicatura3Js/TSLeccion01/01-03-ciclos.js
@@ -21,3 +21,12 @@
  }
  console.log("Fin del ciclo for")
 
+//Palabra reservada break
+for(let contando = 0 ; contando <= 10; contando ++){
+    if(contando % 2 == 0){
+        console.log(contando);//Muestra todos los pares
+        break
+    }
+
+}
+console.log("Termina el ciclo al encontrar el primer número par ")
