8b32f67c43857e42117b2ad400bff78b3fe19cf9
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-20 20:48:33 -0300

    Array

--- Tecnicatura3Js/TsLeccion02/02-03-arreglos.js
+++ Tecnicatura3Js/TsLeccion02/02-03-arreglos.js
@@ -26,5 +26,9 @@ console.log(autos);
 autos[6] = 'Renaul';
 console.log(autos);
 
+//Como preguntar si es una Array o Arreglo
+console.log(Array.isArray(autos)); //Devuelve un booleano
+
+console.log(autos instanceof Array);//Preguntamos si la variable es una instancia de la clase Array
 
 
