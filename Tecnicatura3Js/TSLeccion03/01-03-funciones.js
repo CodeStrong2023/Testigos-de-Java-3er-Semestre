d0e6cb76a384a77696f98e2d61aced998f8181d3
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-21 10:26:55 -0300

    Tipo de datos en una Funcion

--- Tecnicatura3Js/TSLeccion03/01-03-funciones.js
+++ Tecnicatura3Js/TSLeccion03/01-03-funciones.js
@@ -19,3 +19,15 @@ console.log(resultado);
 (function(a,b){
     console.log('Ejecutando la función:'+(a+b));
 })(9,6);
+
+console.log(typeof miFuncion);
+function miFuncionDos(a,b){
+    console.log (arguments.length);
+}
+
+miFuncionDos(5,7,3,6);
+
+//toString
+var miFuncionTexto = miFuncionDos.toString();
+console.log(miFuncionTexto);
+
