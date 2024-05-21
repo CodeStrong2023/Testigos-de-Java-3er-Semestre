a16c1caf89b75a8bee3f866ad804b1ae02492019
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-21 09:55:32 -0300

    Funciones tipo expresion

--- Tecnicatura3Js/TSLeccion03/01-03-funciones.js
+++ Tecnicatura3Js/TSLeccion03/01-03-funciones.js
@@ -9,3 +9,8 @@ miFuncion(5,4);
 
 let resultado =miFuncion(6,7);
 console.log(resultado);
+
+//Declaramos una funcion de tipo expresión o anonima
+let x = function(a, b){return a + b};//necesita cierre con punto y coma
+resultado = x (5 , 6);//al llamarla se pone la variable y parentesis 
+console.log(resultado);
