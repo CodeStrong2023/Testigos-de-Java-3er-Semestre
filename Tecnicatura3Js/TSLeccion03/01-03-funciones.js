0ac6868f9e29f6b2c8dbbe38d14ab12673334da9
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-21 09:38:21 -0300

    Funciones(palabra return)

--- Tecnicatura3Js/TSLeccion03/01-03-funciones.js
+++ Tecnicatura3Js/TSLeccion03/01-03-funciones.js
@@ -1,7 +1,11 @@
 miFuncion(8,2);//Esto se le conoce como hosting
 
 function miFuncion(a, b){
-    console.log("Sumamos: "+ (a+b));
+    //console.log("Sumamos: "+ (a+b));
+    return a + b;
 }
 //Llamando la función
 miFuncion(5,4);
+
+let resultado =miFuncion(6,7);
+console.log(resultado);
