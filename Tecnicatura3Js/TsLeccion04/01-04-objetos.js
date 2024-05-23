951eb7aaa29f3eb460a85baffc9d4ede99e1fb63
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-23 10:58:06 -0300

    Objeto

---
+++ Tecnicatura3Js/TsLeccion04/01-04-objetos.js
@@ -0,0 +1,12 @@
+let x = 10; //variable de tipo primitiva
+console.log(x.length);
+
+//Objeto
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.como',
+    edad: 30
+}
+
+console.log(persona.nombre);
