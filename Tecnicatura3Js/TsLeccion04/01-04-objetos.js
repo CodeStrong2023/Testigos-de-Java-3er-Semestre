2a11989caf187003abd93edde31efaad97358586
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-29 12:00:34 -0300

    Ejecutamos desde el navegador

--- Tecnicatura3Js/TsLeccion04/01-04-objetos.js
+++ Tecnicatura3Js/TsLeccion04/01-04-objetos.js
@@ -1,6 +1,6 @@
 let x = 10; //variable de tipo primitiva
 console.log(x.length);
-
+console.log('Tipos primitivos');
 //Objeto
 let persona = {
     nombre: 'Carlos',
@@ -18,20 +18,21 @@ console.log(persona.email);
 console.log(persona.edad);
 console.log(persona);
 console.log(persona.nombreCompleto());
-
+console.log('Ejecutando con un objeto');
 let persona2 =new Object();
 persona2.nombre='Juan';
 persona2.direccion='Salada 14';
 persona2.telefono='5492618282821';
 console.log(persona2.telefono);
-
+console.log('Creamos un nuevo objeto');
 console.log(persona['apellido']);//Accedemos como si fuera un arregle
-
+console.log('Usamos el ciclo for in');
 //fot in y accedemos al objeto como si fuera un arreglo
 for(propiedad in persona){
     console.log(propiedad);
     console.log(persona[propiedad]);
 }
+console.log('cambiamos y eliminamos un error');
 persona.apellida = 'Betancud';//Cambiamos dinamicamente un valor del objeto
 delete persona.apellida;//Eliminamos el error
 console.log(persona);
