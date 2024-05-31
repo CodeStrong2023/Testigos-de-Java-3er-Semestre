4fbd6a64967a3916c7b77fbff11a717aeec893be
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-05-31 11:55:15 -0300

    Agregamos metodos al objeto

--- Tecnicatura3Js/TsLeccion04/01-04-objetos.js
+++ Tecnicatura3Js/TsLeccion04/01-04-objetos.js
@@ -78,11 +78,15 @@ console.log(persona.lang);
 function Persona3(nombre,apellido,email){//constructor
     this.nombre = nombre;
     this.apellido = apellido;
-    this.amail =email;
+    this.amail = email;
+    this.nombreCompleto = function(){
+        return this.nombre+' '+this.apellido;
+    }
 }
 let padre= new Persona3('Leo','Lopez','lopez@mail.com');
-padre.nombre='Luis';
+padre.nombre='Luis';//Modificamos el nombre
 console.log(padre);
-
+console.log(padre.nombreCompleto());
 let madre =new Persona3('Laura','Contrera','contreral@gmail.com');
 console.log(madre);
+console.log(madre.nombreCompleto());
