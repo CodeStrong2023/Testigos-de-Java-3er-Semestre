0553308ac87dd779134c6fa8888c0649c0f7ddff
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-05 09:55:52 -0300

    Metodo apply

--- Tecnicatura3Js/TsLeccion04/01-04-objetos.js
+++ Tecnicatura3Js/TsLeccion04/01-04-objetos.js
@@ -138,7 +138,9 @@ let persona4 ={
     apellido: 'Perez',
     nombreCompleto2:function(titulo,telefono){
         return titulo+': '+this.nombre+' '+this.apellido+' '+telefono;
+        //return this.nombre+' '+this.apellido;
     }
+
 }
 
 let persona5={
@@ -146,5 +148,9 @@ let persona5={
     apellido: 'Lara'
 }
 
-console.log(persona4.nombreCompleto2('Lic.','5492618484845'));
+console.log(persona4.nombreCompleto2('Lic.','549261838338'));
 console.log(persona4.nombreCompleto2.call(persona5,'Ing.','5492618585856'));
+
+//Método Apply
+let arreglo = ['Ing. ','5492318686865' ];
+console.log( persona4.nombreCompleto2.apply(persona5,arreglo));
