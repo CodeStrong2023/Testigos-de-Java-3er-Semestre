992692ddb9e45c09c8a6e131623a788ed3953b1a
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-05 09:26:49 -0300

    Metodo call

--- Tecnicatura3Js/TsLeccion04/01-04-objetos.js
+++ Tecnicatura3Js/TsLeccion04/01-04-objetos.js
@@ -131,3 +131,20 @@ console.log(madre.telefono);
 madre.telefono='5492418389823';
 console.log(madre.telefono);
 
+
+// Uso de call 
+let persona4 ={
+    nombre: 'Juan',
+    apellido: 'Perez',
+    nombreCompleto2:function(titulo,telefono){
+        return titulo+': '+this.nombre+' '+this.apellido+' '+telefono;
+    }
+}
+
+let persona5={
+    nombre: 'Carlos',
+    apellido: 'Lara'
+}
+
+console.log(persona4.nombreCompleto2('Lic.','5492618484845'));
+console.log(persona4.nombreCompleto2.call(persona5,'Ing.','5492618585856'));
