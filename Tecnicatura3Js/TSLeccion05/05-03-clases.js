95120891b5235fccd005e374e2d4fc6478a1fc8e
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-06 11:08:13 -0300

    Metodo get y set jscript

--- Tecnicatura3Js/TSLeccion05/05-03-clases.js
+++ Tecnicatura3Js/TSLeccion05/05-03-clases.js
@@ -1,11 +1,25 @@
 class Persona{
     constructor(nombre,apellido){
-        this.nombre= nombre;
-        this.apellido= apellido;
+        this._nombre= nombre;
+        this._apellido= apellido;
     }
+    get nombre(){
+        return this._nombre;
+    }
+    
+    set nombre(nombre){
+        this._nombre=nombre
+    }
+   
 }
 
 let persona1= new Persona('Martin','Perez');
-console.log(persona1);
+console.log(persona1.nombre);
+persona1.nombre= 'Juan carlos';
+console.log(persona1.nombre);
+//console.log(persona1);
 let persona2= new Persona ('Carlos','Lara');
-console.log(persona2);
+console.log(persona2.nombre);
+persona2.nombre='Maria Laura';
+console.log(persona2.nombre);
+//console.log(persona2);
