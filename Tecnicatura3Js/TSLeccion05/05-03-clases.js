0606f7f441a0b545d20fe3694e3fb18e79b482ec
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-06 19:52:40 -0300

    estaticos y no estaticos

--- Tecnicatura3Js/TSLeccion05/05-03-clases.js
+++ Tecnicatura3Js/TSLeccion05/05-03-clases.js
@@ -1,9 +1,15 @@
 //let persona3=new Persona('Carla','Ponce');esto no se debe hacer:Persona is not defined
 
 class Persona{//Calse padre
+    
+    static contadorObjetosPersona=0;
+    email='valor default email';//Atributo no estatico
+
     constructor(nombre,apellido){
         this._nombre= nombre;
         this._apellido= apellido;
+        Persona.contadorObjetosPersona++;
+        console.log('Se incrementa el contador: '+Persona.contadorObjetosPersona);
     }
     get nombre(){
         return this._nombre;
@@ -90,3 +96,11 @@ Persona.saludar2(persona1);
 
 Empleado.saludar();
 Empleado.saludar2(empleado1);
+
+//console.log(persona1.contadorObjetosPersona);
+console.log(Persona.contadorObjetosPersona);
+console.log(Empleado.contadorObjetosPersona);
+
+console.log(persona1.email);
+console.log(empleado1.email);
+//console.log(Persona.email);No puede acceder desde la clase
