af9e1d240dedc9c2a3dd2ba2c61949df0a2ae7e0
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-25 14:06:42 -0300

    leer archivo

--- Tecnicatura3Py/Archivos/Leccion02/leer_archivo.py
+++ Tecnicatura3Py/Archivos/Leccion02/leer_archivo.py
@@ -5,7 +5,16 @@ archivo = open ('prueba.txt','r',encoding='utf8') #las letras son : 'r' read,'a'
 #print(archivo.readline())
 #print(archivo.readline())
 
+
 #Vamos a interar el archivo ,cada una de las líneas
 #for linea in archivo:
     #print(linea): iteramos todos los elementos del archivo
-print(archivo.readlines()[11])#accedemos al archivo como si fuera una lista 
+# print(archivo.readlines()[11])#accedemos al archivo como si fuera una lista 
+
+# Anexamos informacion 
+archivo2=open('copia.txt','w',encoding='utf8')
+archivo2.write(archivo.read())
+archivo.close()# cerramos el primer archivo
+archivo2.close()# cerramos el segundo archivo
+
+print('Se ha terminado el proceso de leer y copiar archivos')
