efbbdfc1c4916c0c4105ece7a305051c31d3b560
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-25 10:13:35 -0300

    Errores

--- Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
+++ Tecnicatura3Py/Archivos/Leccion02/manejo_archivos.py
@@ -1,7 +1,12 @@
 # Declaramos una variable
 try:
-    archivo= open('prueba.txt','w')  #La W es de write que en ingles significa escribir
+    archivo= open('prueba.txt','w',encoding='utf8')  #La W es de write que en ingles significa escribir
+    archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n')
+    archivo.write('Los acentos son importantes para las palabras\n')
+    archivo.write('como por ejemplo:acción,ejecución y pruducción\n')
+    archivo.write('Con esto terminamos')
 except Exception as e:
     print(e)
 finally: # siempre se ejecuta
     archivo.close() #Con esto se debe cerrar el archivo 
+#archivo.write('Todo quedo perfecto'):este es un error
