9aa7f414190c0f44c196ce72d4323e4f05b57ad2
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-06-25 09:48:32 -0300

    ExcepcionesManejo

--- Tecnicatura3Py/Leccion01/manejo_excepciones.py
+++ Tecnicatura3Py/Leccion01/manejo_excepciones.py
@@ -1,4 +1,22 @@
+from NumerosIgualesException import NumerosIgualesException
+resultado= None
+
 try:
-    10/0
+    a=int(input('Digite el primer número: '))
+    b=int(input('Digite el segundo número: '))
+    if a == b :
+        raise NumerosIgualesException('Son numeros iguales')
+    resultado=a/b # modificamos
+except TypeError as e:
+    print(f'TypeError -Ocurrio un error:{type(e)}')
+except ZeroDivisionError as e:
+    print(f'ZeroDivisionError- Ocurrio un error:{type(e)}')
 except Exception as e:
-    print(f'Ocurrio un error:{e}')
+    print(f'Exception- Ocurrio un error:{type(e)}')
+else:
+    print("No se arrojo nunguna excepción")
+finally:#Siempre se va a ejecutar
+    print("Ejecucion de este bloque finally")
+
+print(f'El resultado es:{resultado}')
+print('seguimos...')
